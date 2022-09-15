0b5ca5c8e4464ab43a9037856090ca8cbedb0da9
Anil <aniljp97@yahoo.com>
2022-09-15 00:14:50 -0500

    problem 118 completed.

--- Euler/p118.cpp
+++ Euler/p118.cpp
@@ -1,99 +1,77 @@
 #include "euler.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
 #include "MyMethods.h"
 
 // https://projecteuler.net/problem=118
 
-//////////////////
-/// UNFINISHED ///
-//////////////////
+/// <summary>
+/// Returns the number of partitions of a the set where all the subsets are prime
+/// </summary>
+/// <param name="start">Keeps track of the set dividing index through the recursive stack</param>
+/// <param name="prev">Saves the previously found subset in the recursive check of one set. The set does not count if the previous subset is greater than the next (to avoid duplicates, ex. {2,47,...} and {47,2,...})</param>
+/// <param name="set">The full number set (never changed)</param>
+/// <param name="res">result as a int passed by reference to make adding up simpler within recursive context</param>
+void getNumSubsetCombos(int start, int prev, vector<int> set, int& res) {
+	int subset = 0;
 
-
-bool uniqueCharacters(string str)
-{
-	int checker = 0;
-
-	for (int i = 0; i < str.length(); i++) {
-
-		int bitAtIndex = str[i] - '0';
-
-		// if that bit is already set in
-		// checker, return false
-		if ((checker & (1 << bitAtIndex)) > 0) {
-			return false;
+	for (; start < set.size(); start++) {
+		subset *= 10;
+		subset += set.at(start);
+		//cout << subset;
+		if (subset > prev && isPrime(subset)) {
+			//cout << " prime" << endl;
+			getNumSubsetCombos(start + 1, subset, set, res);
 		}
-
-		// otherwise update and continue by
-		// setting that bit in the checker
-		checker = checker | (1 << bitAtIndex);
+		//cout << endl;
 	}
 
-	// no duplicates encountered, return true
-	return true;
-}
-
-int numPrimeCombos(int i) {
-	int res = 0;
-	string i_str = to_string(i);
-
-	char setLastChar = i_str.back();
-	if (setLastChar == '8' || setLastChar == '6' || setLastChar == '4') { // if the last digit is an even number (other than 2), the sets last digit will be a non-prime
-		return 0;
-	}
-
-	if (setLastChar == 2) { // when the last digit is 2 the only way the last digit can be a prime is as 2. So the digit 2 will need to be by itself
-		string i_wo_two = i_str.substr(0, i_str.length() - 1);
-	}
-
-	// because the loop below is using bit shifting, we can't start on 0 so we can just check if the full number is prime
-	if (isPrime(i)) {
+	if (start == set.size() && subset > prev && isPrime(subset)) {
+		//cout <<  "FIN: " << subset << endl;
 		res++;
 	}
+}
 
-	// using iterating all of the possible comma placement to make a set through 1 byte a binary, a 1 representing a comma
-	for (unsigned short int plcmnt = 1; plcmnt <= 255;) {
-		string str_plc = getBinary(plcmnt, 8);
-		int prev_c_ind = 0;
-		for (int c_ind = 1; c_ind <= 9; c_ind++) {
-			if (str_plc[c_ind - 1] == '1' || c_ind == 9) {
-				int sub_num = stoi(i_str.substr(prev_c_ind, c_ind));
-				if (!isPrime(sub_num)) {
-					plcmnt <<= 1;
-					goto continuePlacement;
-				}
+void getCombinationsRecursive(vector<int> combo, vector<int> numsToAdd, int& res) {
 
-				if (c_ind == 9) {
-					res++;
-				}
+	if (combo.size() == 2) {
+		cout << combo[0] << combo[1] << "%" << endl;
+	}
 
-				prev_c_ind = c_ind;
-			}
+	if (numsToAdd.size() == 0 ) {
+		/*for (int c : combo) {
+			cout << c;
+		}*/
+		int lastNum = combo.back();
+		if (lastNum != 4 && lastNum != 6 && lastNum != 8) {
+			// Get each combination of subsets of the combo number
+
+			int subsets = 0;
+			getNumSubsetCombos(0, 0, combo, subsets);
+			//cout << " - " << subsets << endl;
+			res += subsets;
 		}
+		/*else {
+			cout << endl;
+		}*/
+		return;
+	}
 
-		plcmnt++;
-
-	continuePlacement:
-		continue;
+	for (int i = 0; i != numsToAdd.size(); i++) {
+		vector<int> allToAdd = numsToAdd;
+		combo.push_back(numsToAdd.at(i));
+		allToAdd.erase(allToAdd.begin() + i);
+		getCombinationsRecursive(combo, allToAdd, res);
+		combo.pop_back();
 	}
 }
 
 int p118() {
-	int res = 0;
-
 	// iterate through all possible integers sets
-	int ind = 8;
-	for (int i = 123456789; i <= 987654321;) {
-		string i_str = to_string(i);
-
-		// do prime check here
-		if (i_str[ind] == 8) {
-			ind--;
-		}
-
-		swap(i_str[ind], i_str[ind + 1]); // maybe figure something with swapping
-	}
-
-
+	int res = 0;
+	getCombinationsRecursive({}, { 1,2,3,4,5,6,7,8,9 }, res);
 	return res;
 }
